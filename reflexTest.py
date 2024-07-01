c90464b42da3c401386b1b7824bf5b2b8e5232a9
zaiahmd <zaiahmd@icloud.com>
2024-07-01 09:27:14 -0400

    Update reflexTest.py
    random list

---
+++ reflexTest.py
@@ -0,0 +1,22 @@
+import keyboard
+
+import random 
+listofKeys = (a,z)
+random.choice(list(listofKeys.values()))
+
+import time 
+start= time.time()
+print("insert letter " + listofKeys:)
+end = time.time()
+print(Results)
+
+import os
+print(Results)
+
+def Results():
+    time
+
+
+
+keyboard.wait() 
+userInput = input("please insert your username, and your birth year: ")
